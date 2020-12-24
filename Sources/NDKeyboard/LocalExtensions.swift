72d77757167b67dd2503f398d45511ac47f3b3a7
Dave Glassco <dave@neone.com>
2020-12-24 16:20:57 -0600

    extension test

---
+++ Sources/NDKeyboard/LocalExtensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  NDKeyboardApp
+//
+//  Created by Dave Glassco on 12/24/20.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+public extension UIApplication {
+    func addTapGestureRecognizer() {
+        guard let window = windows.first else { return }
+        let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = self
+        tapGesture.name = "MyTapGesture"
+        window.addGestureRecognizer(tapGesture)
+    }
+ }
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false // set to `false` if you don't want to detect tap during other gestures
+    }
+}
+
+
+public extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
